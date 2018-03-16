9d81b61a810aa03762bd4220fb6a57848221455f
quadrrem <quadrelli@localhost>
2018-03-16 12:19:19 +0100

    IU-2017.3.5 <quadrelli@Remos-MBP.int.abacus.ch Create markdown.xml, IntelliLang.xml, project.default.xml, diff.xml, equalsHashCodeTemplates.xml, Interface.java, RemoJava.xml, Default.xml, file.template.settings.xml, ignore.xml, find.xml, File Header.java, gradle.run.settings.xml, code.style.schemes.xml, _@user_Darcula.icls, path.macros.xml, mavenVersion.xml, tests.xml, Default.xml, debugger.renderers.xml, plain.xml, encoding.xml, editor.codeinsight.xml, keymap.xml, editor.xml, applicationLibraries.xml, debugger.xml, baseRefactoring.xml, abacus-merge.xml, ide.general.xml, CodeGlance.xml, other.xml, xpath.xml, code.style.schemes, ui.lnf.xml, filetypes.xml, projectView.xml, Java.xml, plugin_ui.xml, colors.scheme.xml, notifications.xml, ABACUS Default.xml.copy, databaseDrivers.xml, RemoJava.xml.copy, output.xml, KeyPromoterXStatistic.xml, parameter.hints.xml, Class.java, jdk.table.xml, JUnit4 Test Class.java, groovy_config.xml, mavenRunHelper.xml, laf.xml, Mac OS X 10_5_ copy.xml, web-browsers.xml, proxy.settings.xml, vcs.xml, Default _4_.xml, databaseSettings.xml, toStringTemplates.xml, LohnTeam.xml, usageView.xml, setterTemplates.xml, sonarlint.xml

---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,12 @@
+/*
+ * Creator:
+ * ${DATE} ${TIME} ${USER}
+ *
+ * Maintainer:
+ * ${DATE} ${TIME} ${USER}
+ *
+ * Last Modification:
+ * $Id: $
+ *
+ * Copyright (c) 2014 ABACUS Research AG, All Rights Reserved
+ */
